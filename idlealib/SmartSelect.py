22b789e6aa37198d1ecd911343aba597b6ff2d3d
lsx <11313213@qq.com>
2022-02-20 10:55:17 +0800

    Feat: 优化双击选中体验

--- idlealib/SmartSelect.py
+++ idlealib/SmartSelect.py
@@ -1,6 +1,5 @@
 '''智能选取'''
 
-# TODO shell中的stdout无法匹配
 
 if __name__ == '__main__':
     import __init__
@@ -12,6 +11,8 @@ import re
 sp = lambda c: eval(c.replace('.',',')) # Good!
 jn = lambda x,y: '%i.%i'%(x,y) # Good!
 lc = lambda s: jn(s.count('\n')+1, len(s)-s.rfind('\n')-1) # Good!
+
+code = lambda s: s.split('#')[0].strip()
 
 
 def FixTextSelect(root):
@@ -26,18 +27,15 @@ def Select(text, c1, c2):
 
 
 def FindParen(s, c1='(', c2=')'):
-    lv = 0
+    n1 = n2 = 0
     for n, c in enumerate(s):
-        if c in c1:
-            lv += 1
-        elif c in c2:
-            lv -= 1
-        if lv == 0:
+        n1 += c in c1
+        n2 += c in c2
+        if n1 and n1 == n2:
             return n
 
 
 def MatchSpan(r, s, n):
-    # TODO 当无匹配时的返回最近结果
     for m in re.finditer(r, s):
         if m.start() <= n <= m.end():
             return m.span()
@@ -48,41 +46,44 @@ def Selecting(e):
     text.tag_remove('hit', '1.0', 'end')
 
     for tag in text.tag_names('current'):
-        if tag in ('COMMENT', 'STRING'): # TODO 注释前的空格
-            st, ed = text.tag_prevrange(tag, 'current' + '+1c') # See: `idlelib.squeezer.Squeezer.squeeze_current_text_event`
-            text.mark_set('insert', st)
-            text.tag_remove('sel', '1.0', 'end')
-            text.tag_add('sel', st, ed)
-            return
+        if tag in ('COMMENT', 'STRING'):
+            st, ed = text.tag_prevrange(tag, 'current+1c') # See: `idlelib.squeezer.Squeezer.squeeze_current_text_event`
+            ed2 = text.index(ed + '-1c')
+            cur = text.index('current')
+            if tag == 'STRING' and cur in (st, ed2) or tag == 'COMMENT' and cur == st:
+                Select(text, st, ed)
+                return
 
     cur = text.index('current') # 当用insert时光标位置为自动选区的最开始
-    # print(cur)
-    ln, col = sp(cur)
+    col = sp(cur)[1]
+    line = text.get('current linestart', 'current lineend')
 
-    ss = text.get(cur+' linestart', 'end').split('\n')
-    line = ss[0]
-    indent = re.match(r' *', line).end()
-    empty = not line.split('#')[0].strip()
+    c = text.get('current') # charset: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+    if c == ':':
+        if code(line[col+1:]):
+            Select(text, 'current', 'current+1c')
+        else:
+            indent = re.match(r' *', line).end()
+            ss = text.get('current linestart+1l', 'end').split('\n')
+            for r, row in enumerate(ss):
+                if code(row[:indent+1]):
+                    break
+            Select(text, 'current linestart', 'current linestart+%dl' % (r + 1))
 
-    if col <= indent: # cursor postion
-        for row in range(1, len(ss)):
-            begin = ss[row] if empty else ss[row][:indent+1] # empty代表无穷缩进
-            if begin.split('#')[0].strip() != '' or empty: # 起始时是empty则只选中一行
-                break
-        Select(text, 'current linestart', 'current linestart+%dl' % row)
-        return
-
-    c = text.get('current')
-    if c == '\n':
+    elif c == '\n':
         Select(text, 'current linestart', 'current linestart+1l')
 
-    elif c == ' ':
+    elif c == ' ': # TODO 前导空格快速选中区块
         p1, p2 = MatchSpan(r' +', line, col)
         Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
 
+    elif c in '\'"': # quote in comment
+        s = text.get('current+1c', 'end')
+        n = s.find(c)
+        Select(text, 'current', 'current+%dc' % (n + 2))
+
     elif c in '([{':
         # idle内自建的方法太复杂 idlelib.hyperparser.HyperParser.get_surrounding_brackets
-        # TODO 字符串内、转义引号的问题
         c1 = c
         c2 = ')]}'['([{'.index(c1)]
         s = text.get('current', 'end')
@@ -96,17 +97,27 @@ def Selecting(e):
         n = FindParen(reversed(s), c1, c2)
         Select(text, 'current-%dc' % n, 'current+1c')
 
-    elif c == ',':
-        # TODO 两个括号的问题：(a, b())
-        s = text.get('current', 'end')
-        for n, c in enumerate(s[1:]):
-            if c in ',{[()]}\n':
-                Select(text, 'current', 'current+%dc' % (n + 1))
+    elif c == '\\': # "\ooo" "\xhh" "\uxxxx" "\Uxxxxxxxx"
+        c2 = text.get('current+1c')
+        n = {'x': 4, 'u': 6, 'U': 10}.get(c2, 2)
+        Select(text, 'current', 'current+%dc' % n)
+
+    elif c == '.':
+        s = text.get('current+1c', 'current lineend')
+        n = re.match('\s*\w*', s).end()
+        Select(text, 'current', 'current+%dc' % (n + 1))
+
+    elif c == ',': # for test: (a, b(c, d=5), e); for a, b in c; a, b = c, 'd'
+        s = text.get('current+1c', 'end')
+        n1 = n2 = 0
+        for n, c1 in enumerate(s):
+            if n1 == n2 and c1 in ',)]}':
                 break
+            n1 += c1 in '([{'
+            n2 += c1 in ')]}'
+        Select(text, 'current', 'current+%dc' % (n + 1))
 
     elif re.match(r'\w', c):
-        # TODO 选中后取消
-        # TODO 一行中第一个word如果是一个字符无法选中（命中另一个规则）
         p1, p2 = MatchSpan(r'\w+', line, col)
         Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
         word = line[p1:p2]
@@ -115,10 +126,10 @@ def Selecting(e):
             p1, p2 = m.span()
             text.tag_add('hit', lc(s[:p1]), lc(s[:p2])) # 如果使用`1.0+nc`字符偏移会命中Squeezer导致错位
 
-    elif re.match(r'\W', c):
-        # 改善“s.split('\n')[:ln]”会匹配”')[:“的糟糕匹配体验
-        p1, p2 = MatchSpan(r'[^\w\(\)\[\]\{\}\'" ]+', line, col) # 不"粘"括号空格和引号
+    else: # charset: !$%&*+,-/;<=>?@^`|~  for test: s.split('\n')[:ln]
+        p1, p2 = MatchSpan(r'[^\w()[\]{}\'" ]+', line, col) # 不"粘"括号空格和引号
         Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
+        # Select(text, 'current', 'current+1c')
 
 
 class SmartSelect:
