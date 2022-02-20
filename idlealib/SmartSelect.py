09d916bc3471c4f70ce2fe7584a3bfccb31d3506
lsx <11313213@qq.com>
2022-02-20 10:45:31 +0800

    Style: 调整缩进

--- idlealib/SmartSelect.py
+++ idlealib/SmartSelect.py
@@ -14,15 +14,15 @@ jn = lambda x,y: '%i.%i'%(x,y) # Good!
 lc = lambda s: jn(s.count('\n')+1, len(s)-s.rfind('\n')-1) # Good!
 
 
+def FixTextSelect(root):
+    tk = root.tk
+    tk.eval('bind Text <Double-1> {catch {%W mark set insert sel.first}}') # See: "~\tcl\tk8.6\text.tcl"
+
+
 def Select(text, c1, c2):
     text.tag_remove('sel', '1.0', 'end')
     text.tag_add('sel', c1, c2)
     text.mark_set('insert', c1)
-
-
-def FixTextSelect(root):
-    tk = root.tk
-    tk.eval('bind Text <Double-1> {catch {%W mark set insert sel.first}}') # See: "~\tcl\tk8.6\text.tcl"
 
 
 def FindParen(s, c1='(', c2=')'):
@@ -70,55 +70,55 @@ def Selecting(e):
             if begin.split('#')[0].strip() != '' or empty: # 起始时是empty则只选中一行
                 break
         Select(text, 'current linestart', 'current linestart+%dl' % row)
+        return
 
-    else:
-        c = text.get('current')
-        if c == '\n':
-            Select(text, 'current linestart', 'current linestart+1l')
+    c = text.get('current')
+    if c == '\n':
+        Select(text, 'current linestart', 'current linestart+1l')
 
-        elif c == ' ':
-            p1, p2 = MatchSpan(r' +', line, col)
-            Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
+    elif c == ' ':
+        p1, p2 = MatchSpan(r' +', line, col)
+        Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
 
-        elif c in '([{':
-            # idle内自建的方法太复杂 idlelib.hyperparser.HyperParser.get_surrounding_brackets
-            # TODO 字符串内、转义引号的问题
-            c1 = c
-            c2 = ')]}'['([{'.index(c1)]
-            s = text.get('current', 'end')
-            n = FindParen(s, c1, c2)
-            Select(text, 'current', 'current+%dc' % (n + 1))
+    elif c in '([{':
+        # idle内自建的方法太复杂 idlelib.hyperparser.HyperParser.get_surrounding_brackets
+        # TODO 字符串内、转义引号的问题
+        c1 = c
+        c2 = ')]}'['([{'.index(c1)]
+        s = text.get('current', 'end')
+        n = FindParen(s, c1, c2)
+        Select(text, 'current', 'current+%dc' % (n + 1))
 
-        elif c in ')]}':
-            c1 = c
-            c2 = '([{'[')]}'.index(c1)]
-            s = text.get('1.0', 'current+1c')
-            n = FindParen(reversed(s), c1, c2)
-            Select(text, 'current-%dc' % n, 'current+1c')
+    elif c in ')]}':
+        c1 = c
+        c2 = '([{'[')]}'.index(c1)]
+        s = text.get('1.0', 'current+1c')
+        n = FindParen(reversed(s), c1, c2)
+        Select(text, 'current-%dc' % n, 'current+1c')
 
-        elif c == ',':
-            # TODO 两个括号的问题：(a, b())
-            s = text.get('current', 'end')
-            for n, c in enumerate(s[1:]):
-                if c in ',{[()]}\n':
-                    Select(text, 'current', 'current+%dc' % (n + 1))
-                    break
+    elif c == ',':
+        # TODO 两个括号的问题：(a, b())
+        s = text.get('current', 'end')
+        for n, c in enumerate(s[1:]):
+            if c in ',{[()]}\n':
+                Select(text, 'current', 'current+%dc' % (n + 1))
+                break
 
-        elif re.match(r'\w', c):
-            # TODO 选中后取消
-            # TODO 一行中第一个word如果是一个字符无法选中（命中另一个规则）
-            p1, p2 = MatchSpan(r'\w+', line, col)
-            Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
-            word = line[p1:p2]
-            s = text.get('1.0', 'end')
-            for m in re.finditer(r'\b%s\b' % word, s):
-                p1, p2 = m.span()
-                text.tag_add('hit', lc(s[:p1]), lc(s[:p2])) # 如果使用`1.0+nc`字符偏移会命中Squeezer导致错位
+    elif re.match(r'\w', c):
+        # TODO 选中后取消
+        # TODO 一行中第一个word如果是一个字符无法选中（命中另一个规则）
+        p1, p2 = MatchSpan(r'\w+', line, col)
+        Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
+        word = line[p1:p2]
+        s = text.get('1.0', 'end')
+        for m in re.finditer(r'\b%s\b' % word, s):
+            p1, p2 = m.span()
+            text.tag_add('hit', lc(s[:p1]), lc(s[:p2])) # 如果使用`1.0+nc`字符偏移会命中Squeezer导致错位
 
-        elif re.match(r'\W', c):
-            # 改善“s.split('\n')[:ln]”会匹配”')[:“的糟糕匹配体验
-            p1, p2 = MatchSpan(r'[^\w\(\)\[\]\{\}\'" ]+', line, col) # 不"粘"括号空格和引号
-            Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
+    elif re.match(r'\W', c):
+        # 改善“s.split('\n')[:ln]”会匹配”')[:“的糟糕匹配体验
+        p1, p2 = MatchSpan(r'[^\w\(\)\[\]\{\}\'" ]+', line, col) # 不"粘"括号空格和引号
+        Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
 
 
 class SmartSelect:
