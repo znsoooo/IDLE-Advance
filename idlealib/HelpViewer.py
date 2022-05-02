e6b28e649c9b1a1c1fb2a490f9eb3c2c87b871d8
lsx <11313213@qq.com>
2022-05-02 14:10:13 +0800

    Add: 获取命名空间的help信息

--- idlealib/HelpViewer.py
+++ idlealib/HelpViewer.py
@@ -7,54 +7,56 @@ if __name__ == '__main__':
 
 
 import io
-import re
 
 import sys
 if sys.version_info > (3, 6):
     from idlelib.textview import view_text
-    from idlelib.calltip import get_entity
 else:
     from idlelib.textView import view_text
-    from idlelib.CallTips import get_entity
 
 
 sp = lambda c: eval(c.replace('.',',')) # Good!
 jn = lambda x,y: '%i.%i'%(x,y) # Good!
 
-
-def HelpText(object):
-    # 在pydoc.ttypager和pydoc.plainpager中使用了一些`sys.stdout.write`的方法，所以hack重定向此方法后读取help文本
-    f = io.StringIO()
-    stdout, sys.stdout = sys.stdout, f
-    help(object)
-    sys.stdout = stdout
-    f.tell()
-    f.seek(0)
-    return f.read()
+cmd = '''if 1:
+    obj = %r
+    try:
+        help(eval(obj))
+    except (NameError, SyntaxError):
+        help(obj)'''
 
 
 class HelpViewer:
     def __init__(self, parent):
         self.text = parent.text
+        self.flist = parent.flist
+
         parent.rmenu.add_separator()
         parent.rmenu.add_command(label='Help Viewer', command=self.view)
         parent.add_adv_menu('Help Viewer', self.view)
 
     def view(self):
-        text = self.text
-        if not text.tag_ranges('sel'):
-            if re.match('\W', text.get('insert')):
-                text.tag_add('sel', 'insert', 'insert+1c')
-            else:
-                ln, col = sp(text.index('insert'))
-                line = text.get('%d.0' % ln, '%d.end' % ln)
-                end = col + re.match('\w+', line[col:]).end()
-                start = re.search('[\w.]+$', line[:end]).start()
-                text.tag_add('sel', jn(ln, start), jn(ln, end))
+        sel = self.text.get('sel.first', 'sel.last')
+        if sel:
+            s = self.help(cmd % sel)
+            view_text(self.text, 'Help on %r:' % sel, s, modal=False)
 
-        sel = text.get('sel.first', 'sel.last')
-        obj = get_entity(sel)
-        if obj is None:
-            obj = sel
+    def help(self, cmd):
+        # open shell
+        shell = self.flist.pyshell or self.flist.open_shell()
+        interp = shell.interp
 
-        view_text(text, 'Help on %r:' % sel, HelpText(obj), modal=False)
+        # fake printer
+        f = io.StringIO()
+        _write = shell.write
+        shell.write = lambda s, tags=(): f.write(s)
+
+        # run command
+        interp.runcommand(cmd)
+
+        # restore
+        shell.write = _write
+
+        # get result
+        f.seek(0)
+        return f.read()
