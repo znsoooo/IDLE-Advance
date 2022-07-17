9fbb4840f5d7dcb9b951641698a59933d683962a
lsx <11313213@qq.com>
2022-07-17 18:16:00 +0800

    Add: 删除当前行

--- idlealib/DuplicateLine.py
+++ idlealib/DuplicateLine.py
@@ -1,4 +1,4 @@
-"""重复选中文本"""
+"""重复选中文本/删除当前行"""
 
 if __name__ == '__main__':
     import __init__
@@ -8,12 +8,18 @@ if __name__ == '__main__':
 class DuplicateLines:
     def __init__(self, parent):
         self.text = parent.text
-        self.text.bind('<Control-D>', self.callback)
+        self.text.bind('<Control-d>', self.copy)
+        self.text.bind('<Control-D>', self.delete)
 
-    def callback(self, evt):
+    def copy(self, evt):
         if not self.text.tag_ranges('sel'):
             self.text.tag_add('sel', 'insert linestart', 'insert+1l linestart')
-        # else:
-        #     self.text.tag_add('sel', 'sel.first linestart', 'sel.last-1c+1l linestart')
         self.text.mark_set('insert', 'sel.first')
         self.text.insert('sel.last', self.text.get('sel.first', 'sel.last'))
+        return 'break'
+
+    def delete(self, evt):
+        if not self.text.tag_ranges('sel'):
+            self.text.tag_add('sel', 'insert linestart', 'insert+1l linestart')
+        self.text.delete('sel.first', 'sel.last')
+        return 'break'
