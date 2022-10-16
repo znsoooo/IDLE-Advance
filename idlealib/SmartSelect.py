6fdde4106338db9c9a023692fcc4ecc574e2893a
lsx <11313213@qq.com>
2022-10-16 21:05:53 +0800

    Fix: 整理代码顺序、选中注释前续空格

--- idlealib/SmartSelect.py
+++ idlealib/SmartSelect.py
@@ -45,14 +45,12 @@ def Selecting(e):
     text = e.widget
     text.tag_remove('hit', '1.0', 'end')
 
-    for tag in text.tag_names('current'):
-        if tag in ('COMMENT', 'STRING'):
-            st, ed = text.tag_prevrange(tag, 'current+1c') # See: `idlelib.squeezer.Squeezer.squeeze_current_text_event`
-            ed2 = text.index(ed + '-1c')
-            cur = text.index('current')
-            if tag == 'STRING' and cur in (st, ed2) or tag == 'COMMENT' and cur == st:
-                Select(text, st, ed)
-                return
+    if 'STRING' in text.tag_names('current'):
+        st, ed = text.tag_prevrange('STRING', 'current+1c') # See: `idlelib.squeezer.Squeezer.squeeze_current_text_event`
+        ed2 = text.index(ed + '-1c')
+        cur = text.index('current')
+        if cur in (st, ed2):
+            return Select(text, st, ed)
 
     cur = text.index('current') # 当用insert时光标位置为自动选区的最开始
     col = sp(cur)[1]
@@ -70,9 +68,6 @@ def Selecting(e):
                     break
             Select(text, 'current linestart', 'current linestart+%dl' % (r + 1))
 
-    elif c == '\n':
-        Select(text, 'current linestart', 'current linestart+1l')
-
     elif c == ' ':
         indent = re.match(r' *', line).end()
         if col < indent:
@@ -84,6 +79,26 @@ def Selecting(e):
         else:
             p1, p2 = MatchSpan(r' +', line, col)
             Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
+
+    elif re.match(r'\w', c):
+        p1, p2 = MatchSpan(r'\w+', line, col)
+        Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
+        word = line[p1:p2]
+        s = text.get('1.0', 'end')
+        for m in re.finditer(r'\b%s\b' % word, s):
+            p1, p2 = m.span()
+            text.tag_add('hit', lc(s[:p1]), lc(s[:p2])) # 如果使用`1.0+nc`字符偏移会命中Squeezer导致错位
+
+    elif c == '\n':
+        Select(text, 'current linestart', 'current+1c')
+
+    elif c == '#': # for # test # text #
+        if 'COMMENT' in text.tag_names('current') and \
+           text.index('current') == text.tag_prevrange('COMMENT', 'current+1c')[0]:
+            p1, p2 = MatchSpan(r' *#', line, col)
+            Select(text, 'current linestart+%dc' % p1, 'current lineend')
+        else:
+            Select(text, 'current', 'current+1c')
 
     elif c in '\'"`': # quote in comment
         s = text.get('current+1c', 'end')
@@ -124,15 +139,6 @@ def Selecting(e):
             n1 += c1 in '([{'
             n2 += c1 in ')]}'
         Select(text, 'current', 'current+%dc' % (n + 1))
-
-    elif re.match(r'\w', c):
-        p1, p2 = MatchSpan(r'\w+', line, col)
-        Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
-        word = line[p1:p2]
-        s = text.get('1.0', 'end')
-        for m in re.finditer(r'\b%s\b' % word, s):
-            p1, p2 = m.span()
-            text.tag_add('hit', lc(s[:p1]), lc(s[:p2])) # 如果使用`1.0+nc`字符偏移会命中Squeezer导致错位
 
     else: # charset: !$%&*+,-/;<=>?@^|~  for test: s.split('\n')[:ln]
         p1, p2 = MatchSpan(r'[^\w()[\]{}\'" ]+', line, col) # 不"粘"括号空格和引号
