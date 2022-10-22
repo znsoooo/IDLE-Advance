aadc1f6aaa5e6c21663ee92156befcd230540c2f
lsx <11313213@qq.com>
2022-10-22 15:14:34 +0800

    Fix: 另存之后获取路径出错

--- idlealib/FileManager.py
+++ idlealib/FileManager.py
@@ -15,45 +15,46 @@ from tkinter.simpledialog import askstring
 
 class FileManager(tk.Menu):
     def __init__(self, parent):
-        text = parent.text
+        self.text = parent.text
+        self.io = parent.io
         root = parent.root
-        io = parent.io
 
         tk.Menu.__init__(self, root, tearoff=0)
-        n = io.filename or os.getcwd() + '\\' # 当新建文件时 io.filename==None 尾部加分隔符避免获取路径出错
-        text_insert   = lambda s: (lambda: text.insert('insert', s))
-        clipboard_set = lambda s: (lambda: (root.clipboard_clear(), root.clipboard_append(s)))
 
-        self.add_command(label='Copy Fullname', command=clipboard_set(n))
-        self.add_command(label='Copy Filename', command=clipboard_set(basename(n)))
-        self.add_command(label='Copy Dirname',  command=clipboard_set(dirname(n)))
+        path = lambda: self.io.filename or os.getcwd() + os.sep # `io.filename` get `None` when in new file
+        copy = lambda s: (root.clipboard_clear(), root.clipboard_append(s))
+        insert = lambda s: self.text.insert('insert', s)
+
+        self.add_command(label='Copy Fullname', command=lambda: copy(path()))
+        self.add_command(label='Copy Filename', command=lambda: copy(basename(path())))
+        self.add_command(label='Copy Dirname',  command=lambda: copy(dirname(path())))
         self.add_separator()
-        self.add_command(label='Insert Fullname', command=text_insert("r'%s'"%n))
-        self.add_command(label='Insert Filename', command=text_insert("'%s'"%basename(n)))
-        self.add_command(label='Insert Dirname',  command=text_insert("r'%s'"%dirname(n)))
+        self.add_command(label='Insert Fullname', command=lambda: insert("r'%s'" % path()))
+        self.add_command(label='Insert Filename', command=lambda: insert("'%s'"  % basename(path())))
+        self.add_command(label='Insert Dirname',  command=lambda: insert("r'%s'" % dirname(path())))
         self.add_separator()
-        self.add_command(label='Open in Explorer', command=lambda: subprocess.Popen('explorer /select, "%s"'%n))
+        self.add_command(label='Open in Explorer', command=lambda: subprocess.Popen('explorer /select, "%s"' % path().replace('/', '\\')))
         self.add_command(label='Open in CMD',      command=lambda: os.system('start'))
         # self.add_command(label='Run in CMD',       command=lambda: os.system('start "%s"'%io.filename)) # TODO
         self.add_separator()
-        self.add_command(label='Rename File', command=lambda: self.Rename(text, io))
-        self.add_command(label='Reload File', command=lambda: self.Reload(io))
+        self.add_command(label='Rename File', command=self.Rename)
+        self.add_command(label='Reload File', command=self.Reload)
 
-        text.bind('<F2>', lambda e: self.Rename(text, io))
+        self.text.bind('<F2>', self.Rename)
 
         parent.amenu.insert_cascade(3, label='File Manager', menu=self)
 
-    def Rename(self, text, io):
-        basename2 = basename(io.filename)
-        filename2 = askstring('Rename', 'Input new filename:', initialvalue=basename2, parent=text) # 允许输入相对路径
+    def Rename(self):
+        basename2 = basename(self.io.filename)
+        filename2 = askstring('Rename', 'Input new filename:', initialvalue=basename2, parent=self.text) # 允许输入相对路径
         if filename2:
             if filename2[-3:].lower() != '.py':
                 filename2 += '.py'
             dirname2 = dirname(filename2)
             if dirname2: # 只输入文件名的时候路径为 ''
                 os.makedirs(dirname2, exist_ok=True)
-            os.rename(io.filename, filename2) # TODO 切换目录时工作路径未变化
-            io.set_filename(os.path.abspath(filename2)) # TODO save状态变化
+            os.rename(self.io.filename, filename2) # TODO 切换目录时工作路径未变化
+            self.io.set_filename(os.path.abspath(filename2)) # TODO save状态变化
 
-    def Reload(self, io): # TODO 重载记忆位置
-        io.loadfile(io.filename)  # TODO rename也可以用这个方法
+    def Reload(self): # TODO 重载记忆位置
+        self.io.loadfile(self.io.filename)  # TODO rename也可以用这个方法
