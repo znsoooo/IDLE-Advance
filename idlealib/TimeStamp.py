4e90447db25c6a8b9ac67e586ad4843940e4c743
Shixian Li <lsx7@sina.com>
2024-05-25 09:22:39 +0800

    Refactor: 在上一行添加时间戳并添加缩进

---
+++ idlealib/TimeStamp.py
@@ -0,0 +1,26 @@
+'''插入时间戳注释'''
+
+
+if __name__ == '__main__':
+    import __init__
+    __init__.test_editor(__file__)
+
+
+import re
+import time
+
+
+class TimeStamp:
+    def __init__(self, parent):
+        self.text = parent.text
+
+        parent.text.bind('<F4>', self.Insert)
+
+    def Insert(self, evt):
+        line = self.text.get('insert linestart', 'insert lineend')
+        indent = re.match(r'^\s*', line).group()
+
+        stamp = time.strftime('# %Y-%m-%d %H:%M:%S')
+        line2 = indent + stamp + '\n'
+
+        self.text.insert('insert linestart', line2)
