c799486c9723180d138bf2777ce09099e8d79597
Shixian Li <lsx7@sina.com>
2024-06-22 15:58:13 +0800

    Fix: 在添加新入口前先移除旧入口，避免某些版本Python已被卸载无法遍历

--- initialize_on_windows.bat
+++ initialize_on_windows.bat
@@ -7,6 +7,7 @@ echo.
 pause
 echo.
 
+py -c "__import__('idlealib.ContextManager').ContextManager.DeleteWindowsMenuAll()"
 for /f %%i in ('py -0') do (
     echo Initial IDLE-Adv Menu on python %%i ...
     pyw %%i -c "__import__('idlealib.ContextManager').ContextManager.AddWindowsMenu()"
