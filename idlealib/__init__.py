9a253b927e0d90c1e4c0fa72798c52c580d140d5
Shixian Li <lsx7@sina.com>
2024-06-22 17:01:54 +0800

    Upload v1.2.10 to PyPI

--- idlealib/__init__.py
+++ idlealib/__init__.py
@@ -21,8 +21,8 @@ EXTENSIONS = os.listdir(os.path.dirname(os.path.abspath(__file__)))
 
 __author__  = 'Shixian Li <lsx7@sina.com>'
 __credits__ = 'See at: https://github.com/znsoooo/IDLE-Advance'
-__date__    = '20240525'
-__version__ = '1.2.9'
+__date__    = '20240622'
+__version__ = '1.2.10'
 
 __all__ = ['run', 'test_editor', 'test_shell', 'PY36']
 
