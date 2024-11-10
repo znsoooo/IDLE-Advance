6fcbf13bcb0296d3b350c2c9de7e5c146bf2476c
Shixian Li <lsx7@sina.com>
2024-11-10 09:46:15 +0800

    Refactor: 设计面向对象的文本选择器

--- idlealib/SmartSelect.py
+++ idlealib/SmartSelect.py
@@ -20,10 +20,26 @@ def FixTextSelect(root):
     tk.eval('bind Text <Double-1> {catch {%W mark set insert sel.first}}') # See: "~\tcl\tk8.6\text.tcl"
 
 
-def Select(text, c1, c2):
-    text.tag_remove('sel', '1.0', 'end')
-    text.tag_add('sel', c1, c2)
-    text.mark_set('insert', c1)
+class TextSelector:
+    def __init__(self, text):
+        self.text = text
+
+    def select(self, idx1, idx2):
+        self.text.tag_remove('sel', '1.0', 'end')
+        self.text.tag_add('sel', idx1, idx2)
+        self.text.mark_set('insert', idx1)
+
+    def select_chars(self, n=1, n2=0):
+        n, n2 = sorted([n, n2])
+        self.select('current%+dc' % n, 'current%+dc' % n2)
+
+    def select_from_line(self, n1, n2):
+        n2 = 'start%+dc' % n2 if isinstance(n2, int) else n2
+        self.select('current linestart+%dc' % n1, 'current line' + n2)
+
+    def select_lines(self, n=1, n2=0):
+        n, n2 = sorted([n, n2])
+        self.select('current linestart%+dl' % n, 'current linestart%+dl' % n2)
 
 
 def FindParen(s, c1='(', c2=')'):
@@ -58,11 +74,13 @@ def SelectBlock(text, first_line=True):
                 break
 
     ln = ln + 2 if first_line else ln + 1
-    Select(text, 'current linestart', 'current linestart+%dl' % ln)
+    text_sel = TextSelector(text)
+    text_sel.select_lines(ln)
 
 
 def Selecting(e):
     text = e.widget
+    text_sel = TextSelector(text)
     text.tag_remove('hit', '1.0', 'end')
 
     if 'STRING' in text.tag_names('current'):
@@ -70,7 +88,7 @@ def Selecting(e):
         ed2 = text.index(ed + '-1c')
         cur = text.index('current')
         if cur in (st, ed2):
-            return Select(text, st, ed)
+            return text_sel.select(st, ed)
 
     cur = text.index('current') # 当用insert时光标位置为自动选区的最开始
     col = sp(cur)[1]
@@ -79,7 +97,7 @@ def Selecting(e):
     c = text.get('current') # charset: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
     if c == ':':
         if is_code(line[col+1:]):
-            Select(text, 'current', 'current+1c')
+            text_sel.select_chars()
         else:
             SelectBlock(text)
 
@@ -93,14 +111,14 @@ def Selecting(e):
             elif re.fullmatch(patt, line):
                 SelectBlock(text)
             else:
-                Select(text, 'current linestart', 'current linestart+1l')
+                text_sel.select_lines()
         else:
             p1, p2 = MatchSpan(r' +', line, col)
-            Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
+            text_sel.select_from_line(p1, p2)
 
     elif re.match(r'\w', c):
         p1, p2 = MatchSpan(r'\w+', line, col)
-        Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
+        text_sel.select_from_line(p1, p2)
         word = line[p1:p2]
         s = text.get('1.0', 'end')
         for m in re.finditer(r'\b%s\b' % word, s):
@@ -108,23 +126,23 @@ def Selecting(e):
             text.tag_add('hit', lc(s[:p1]), lc(s[:p2])) # 如果使用`1.0+nc`字符偏移会命中Squeezer导致错位
 
     elif c == '\n':
-        Select(text, 'current linestart', 'current+1c')
+        text_sel.select_lines()
 
     elif c == '#': # for # test # text #
         if 'COMMENT' in text.tag_names('current') and \
            text.index('current') == text.tag_prevrange('COMMENT', 'current+1c')[0]:
             p1, p2 = MatchSpan(r' *#', line, col)
             if p1 > 0:
-                Select(text, 'current linestart+%dc' % p1, 'current lineend')
+                text_sel.select_from_line(p1, 'end')
             else:
-                Select(text, 'current linestart', 'current linestart+1l')
+                text_sel.select_lines()
         else:
-            Select(text, 'current', 'current+1c')
+            text_sel.select_chars()
 
     elif c in '\'"`': # quote in comment
         s = text.get('current+1c', 'end')
         n = s.find(c)
-        Select(text, 'current', 'current+%dc' % (n + 2))
+        text_sel.select_chars(n + 2)
 
     elif c in '([{':
         # idle内自建的方法太复杂 idlelib.hyperparser.HyperParser.get_surrounding_brackets
@@ -132,24 +150,24 @@ def Selecting(e):
         c2 = ')]}'['([{'.index(c1)]
         s = text.get('current', 'end')
         n = FindParen(s, c1, c2)
-        Select(text, 'current', 'current+%dc' % (n + 1))
+        text_sel.select_chars(n + 1)
 
     elif c in ')]}':
         c1 = c
         c2 = '([{'[')]}'.index(c1)]
         s = text.get('1.0', 'current+1c')
         n = FindParen(reversed(s), c1, c2)
-        Select(text, 'current-%dc' % n, 'current+1c')
+        text_sel.select_chars(-n, 1)
 
     elif c == '\\': # "\ooo" "\xhh" "\uxxxx" "\Uxxxxxxxx"
         c2 = text.get('current+1c')
         n = {'x': 4, 'u': 6, 'U': 10}.get(c2, 2)
-        Select(text, 'current', 'current+%dc' % n)
+        text_sel.select_chars(n)
 
     elif c == '.':
         s = text.get('current+1c', 'current lineend')
-        n = re.match('\s*\w*', s).end()
-        Select(text, 'current', 'current+%dc' % (n + 1))
+        n = re.match(r'\s*\w*', s).end()
+        text_sel.select_chars(n + 1)
 
     elif c == ',': # for test: (a, b(c, d=5), e); for a, b in c; a, b = c, 'd'
         s = text.get('current+1c', 'end')
@@ -159,12 +177,12 @@ def Selecting(e):
                 break
             n1 += c1 in '([{'
             n2 += c1 in ')]}'
-        Select(text, 'current', 'current+%dc' % (n + 1))
+        text_sel.select_chars(n + 1)
 
     else: # charset: !$%&*+,-/;<=>?@^|~  for test: s.split('\n')[:ln]
         p1, p2 = MatchSpan(r'[^\w()[\]{}\'" ]+', line, col) # 不"粘"括号空格和引号
-        Select(text, 'current linestart+%dc' % p1, 'current linestart+%dc' % p2)
-        # Select(text, 'current', 'current+1c')
+        text_sel.select_from_line(p1, p2)
+        # text_sel.select_chars()
 
 
 class SmartSelect:
